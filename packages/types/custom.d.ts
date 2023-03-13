582f3b863eb848ff9675ac03b793d8515667cc1d
xywc-s <xywc_s@foxmail.com>
2023-03-13 22:53:26 +0800

    types

--- packages/types/custom.d.ts
+++ packages/types/custom.d.ts
@@ -1,1 +1,6 @@
 export declare type Lazy<T> = () => Promise<T>
+export declare type LazyReturnType<T extends (...args: any) => Promise<any>> = T extends (
+  ...args: any
+) => Promise<infer R>
+  ? R
+  : Promise<any>
