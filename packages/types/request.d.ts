a6ac2746e53a77b2ade836947820faeb21e45d54
易长松 <xywc_s@foxmail.com>
2024-05-24 10:15:20 +0800

    新增: 新接口响应类型和查询参数类型

--- packages/types/request.d.ts
+++ packages/types/request.d.ts
@@ -8,24 +8,30 @@ export declare interface AngelResponse<D = any, O = any> {
   footer: any[]
 }
 
+export declare interface AngelResponse2<D = any> {
+  success: boolean
+  message: string
+  data: D | D[] | null
+  total: number
+  code: number
+  footer: any[]
+}
+
 export declare type Pager = {
-  /**
-   * 页码
-   */
+  /** 页码 */
   page: number
-  /**
-   * 每页数据量
-   */
+  /** 每页数据量 */
   rows: number
 }
 
 export declare type Sort = {
-  /**
-   * 排序字段名
-   */
+  /** 排序字段名 */
   sort: string
-  /**
-   * 排序方法
-   */
+  /** 排序方法  */
   order: 'asc' | 'desc'
+}
+
+export declare interface SearchParams extends Pager, Sort {
+  /** 返回数据结果集的字段名, 默认rows, 新接口可选 data */
+  dataKey: 'rows' | 'data'
 }
