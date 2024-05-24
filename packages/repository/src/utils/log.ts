6176df59841808c2fbb964a96a4d7398707cc73d
易长松 <xywc_s@foxmail.com>
2024-05-24 10:18:58 +0800

    增加一个log样式

---
+++ packages/repository/src/utils/log.ts
@@ -0,0 +1,13 @@
+export function lLog(d: any) {
+  console.log(
+    '%c %c %c %c app has mounted! %c %c %c %o',
+    'background:#41b883 ; padding: 1px; margin: 0 1px',
+    'background:#41b883 ; padding: 1px; margin: 0 1px',
+    'background:#41b883 ; padding: 1px; margin: 0 1px',
+    'background:#41b883 ; padding: 1px; margin: 0 1px;  color: #fff',
+    'background:#41b883 ; padding: 1px; margin: 0 1px',
+    'background:#41b883 ; padding: 1px; margin: 0 1px',
+    'background:#41b883 ; padding: 1px; margin: 0 1px',
+    d
+  )
+}
