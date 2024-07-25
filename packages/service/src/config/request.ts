7429382cd4efd9704e68acaab498535c6f204de5
易长松 <xywc_s@foxmail.com>
2024-07-25 15:10:55 +0800

    配置调整

---
+++ packages/service/src/config/request.ts
@@ -0,0 +1,46 @@
+import type { AxiosRequestConfig } from 'axios'
+import type { FetchOptions } from '@angelyeast/repository'
+
+export interface Options<P, T> extends FetchOptions<P, T> {
+  /** 是否立即获取数据 */
+  immediately: boolean
+}
+
+export interface AngelMicroServeRequestConfig<D = any> extends AxiosRequestConfig<D> {
+  /**
+   * 是否启用微服务鉴权
+   * @default true
+   */
+  serveAuth?: boolean
+  /**
+   * 是否全量返回axios的响应
+   * @description 由于微服务响应码全部是200, 状态和消息全部自定义, 所以默认只返回response.data
+   * @default false
+   */
+  fullReturn?: boolean
+}
+
+export const ContentTypes = {
+  FORM: 'application/x-www-form-urlencoded',
+  JSON: 'application/json',
+  UPLOAD: 'multipart/form-data',
+  DOWNLOAD: 'application/octet-stream'
+}
+
+export const form: AxiosRequestConfig = {
+  headers: {
+    'Content-Type': ContentTypes.FORM
+  }
+}
+
+export const json: AxiosRequestConfig = {
+  headers: {
+    'Content-Type': ContentTypes.JSON
+  }
+}
+export const common: AngelMicroServeRequestConfig = {
+  ...form,
+  serveAuth: true,
+  timeout: 20000,
+  withCredentials: false
+}
---
+++ packages/service/src/config/service.ts
@@ -0,0 +1,24 @@
+// 微服务
+import * as Auth from '../auth'
+import * as BFF from '../bff'
+import * as MDM from '../mdm'
+import * as MMS from '../mms'
+import * as Market from '../market'
+import * as OPEN from '../open'
+
+export const ServiceMap = {
+  BFF,
+  /** 鉴权中心 */
+  Auth,
+  /** 主数据库 */
+  MDM,
+  /** 物料管理系统 */
+  MMS,
+  /** 营销 */
+  Market,
+  /** 开放服务 */
+  OPEN
+}
+
+export type Services = Readonly<typeof ServiceMap>
+export type ServiceName = keyof Services
