3784f992af366aa701078a240893d00f5cceb6b5
易长松 <xywc_s@foxmail.com>
2024-08-13 10:28:02 +0800

    更新fetch为异步函数,并返回接口响应数据

--- packages/repository/src/common/fetch.ts
+++ packages/repository/src/common/fetch.ts
@@ -3,7 +3,7 @@ import { isRef } from 'vue'
 import { isFunction } from 'lodash-es'
 import type { Ref } from 'vue'
 import type { AngelResponse, Lazy, LazyReturnType } from '@angelyeast/types'
-import 'element-plus/theme-chalk/el-message.css'
+import 'element-plus/es/components/message/style/index.mjs'
 
 export type LoadingInstance = ReturnType<typeof ElLoadingService>
 export type FetchLoading = Ref<boolean> | ((b: boolean) => void) | LoadingInstance
@@ -31,31 +31,31 @@ export interface FetchOptions<P, T> {
  * @param fn 接口函数
  * @param options 选项参数
  */
-export function useFetch<F extends Lazy>(
+export async function useFetch<F extends Lazy>(
   fn: F,
   options?: Partial<FetchOptions<Parameters<F>, LazyReturnType<F>>>
 ) {
   options = Object.assign({ autoNotify: true }, options)
   options.loading && checkAndToggleLoading(options.loading, true)
   const f = () => (options?.params ? fn(...(options!.params as any[])) : fn())
-  f()
-    .then((res) => {
-      const { success, message } = res as AngelResponse
-      if (success) {
-        options?.autoNotify && message && ElMessage.success(message)
-        if (options?.onSuccess) options?.onSuccess(res)
-      } else {
-        message && ElMessage.error(message)
-        if (options?.onError) options?.onError(res)
-      }
-    })
-    .catch((error) => {
-      if (options?.onError) options?.onError(error)
-    })
-    .finally(() => {
-      options?.loading && checkAndToggleLoading(options.loading, false)
-      if (options?.onFinally) options?.onFinally()
-    })
+  try {
+    const res = await f()
+    const { success, message } = res as AngelResponse
+    if (success) {
+      options?.autoNotify && message && ElMessage.success(message)
+      if (options?.onSuccess) options?.onSuccess(res)
+    } else {
+      message && ElMessage.error(message)
+      if (options?.onError) options?.onError(res)
+    }
+    options?.loading && checkAndToggleLoading(options.loading, false)
+    if (options?.onFinally) options?.onFinally()
+    return res
+  } catch (error) {
+    if (options?.onError) options?.onError(error)
+    options?.loading && checkAndToggleLoading(options.loading, false)
+    if (options?.onFinally) options?.onFinally()
+  }
 }
 
 function checkAndToggleLoading(loading: FetchLoading, value: boolean) {
