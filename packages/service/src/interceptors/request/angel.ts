6240978be4f2861619e2d666751d04290867f7d2
易长松 <xywc_s@foxmail.com>
2024-08-15 14:56:25 +0800

    chore: release v2.1.0

--- packages/service/src/interceptors/request/angel.ts
+++ packages/service/src/interceptors/request/angel.ts
@@ -4,15 +4,8 @@ import type { AngelMicroServeRequestConfig } from '../../config'
 export default {
   success: (config: InternalAxiosRequestConfig & AngelMicroServeRequestConfig) => {
     if (!config.serveAuth) return config
-    const userStr = localStorage.getItem('user')
-    if (userStr) {
-      try {
-        const user = JSON.parse(userStr)
-        config.headers.Authorization = user.access_token
-      } catch (error) {
-        console.error(error)
-      }
-    }
+    const token = localStorage.getItem('Middle-Api-Token')
+    if (token) config.headers.Authorization = token
     return config
   },
   error: (error: any) => {
