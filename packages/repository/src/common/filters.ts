60612addc82fd7047394851cd9e05233fea4a216
易长松 <xywc_s@foxmail.com>
2024-08-28 10:42:41 +0800

    chore: release v2.2.4

--- packages/repository/src/common/filters.ts
+++ packages/repository/src/common/filters.ts
@@ -1,22 +1,19 @@
 import { cloneDeep } from 'lodash-es'
 import { ref, unref } from 'vue'
-import type { Ref, UnwrapRef } from 'vue'
 
 /**
  * 查询条件及重置查询
  * @param data 查询条件对象
  */
-export function useFilters<T extends object>(data: T): [Ref<UnwrapRef<T>>, () => void] {
+export function useFilters<T extends object>(data: T) {
+  /** 响应式查询条件对象 */
+  const filters = ref<T>()
   const _clone = cloneDeep(unref(data))
-  /**
-   * 查询条件对象的ref
-   */
-  const filters = ref(data)
-  /**
-   * 重置查询条件
-   */
+
+  /** 重置查询条件 */
   const reset = () => {
-    filters.value = ref(_clone).value
+    filters.value = cloneDeep(_clone)
   }
-  return [filters, reset]
+  reset()
+  return [filters, reset] as [typeof filters, typeof reset]
 }
